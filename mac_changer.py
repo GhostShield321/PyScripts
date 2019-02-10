8ea7907f1bdac89b6f3b1f26501b6073d64cac4c
Othmane Ait Bouftass <41711147+OthmaneAitBouftass@users.noreply.github.com>
2019-02-10 00:30:22 +0000

    Update mac_changer.py

--- mac_changer.py
+++ mac_changer.py
@@ -3,7 +3,7 @@
 import os
 Interface = input("-Interface: ")
 Mac = input("-New MAC Address : ")
-os.system("ifconfig " + Interface + "down")
-os.system("ifconfig " + Interface + "hw ether " + Mac)
-os.system("ifconfig " + Interface + "up")
+os.system("ifconfig " + Interface + " down")
+os.system("ifconfig " + Interface + " hw ether " + Mac)
+os.system("ifconfig " + Interface + " up")
 print("[+] Your MAC Address Has Successfully Changed To => %s" % (Mac))
